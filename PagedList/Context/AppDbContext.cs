41ed9f2360eb0703c842f6d8255cea7806c374cc
Gustavo Irano <gustavo.irano@hotmail.com>
2020-06-23 22:22:13 -0300

    Adicionar arquivos de projeto.

---
+++ PagedList/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PagedList.Models;
+
+namespace PagedList.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ PagedList/Controllers/ClientesController.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PagedList.Context;
+using PagedList.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace PagedList.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clientes
+        public async Task<IActionResult> Index(int? pagina)
+        {
+            const int itensPorPagina = 2;
+            int numeroPagina = (pagina ?? 1);
+
+            return View(await _context.Clientes.
+                ToPagedListAsync(numeroPagina, itensPorPagina));
+        }
+
+        public async Task<IActionResult> GridPagination(int? pagina)
+        {
+            const int itensPorPagina = 2;
+            int numeroPagina = (pagina ?? 1);
+
+
+            var clientes = await _context.Clientes.
+                ToPagedListAsync(numeroPagina, itensPorPagina);
+
+            return PartialView("_listagemClientes", clientes);
+        }
+
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .FirstOrDefaultAsync(m => m.ClienteId == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClienteId,Nome,Endereco,Email")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClienteId,Nome,Endereco,Email")] Cliente cliente)
+        {
+            if (id != cliente.ClienteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.ClienteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .FirstOrDefaultAsync(m => m.ClienteId == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Clientes.Any(e => e.ClienteId == id);
+        }
+    }
+}
